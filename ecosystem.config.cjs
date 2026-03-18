e9e9bb334e2a9b015c50283c90bd93f3fdc96191
mawegrzyn-ux <ma.wegrzyn@gmail.com>
2026-03-18 23:21:33 +0000

    fixing setup file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name:         'macaroonie-api',
-      script:       './api/src/server.js',
-      instances:    'max',
-      exec_mode:    'cluster',
-      watch:        false,
-      max_memory_restart: '512M',
-      interpreter_args: '--import ./api/src/preload.js',
-      env: { NODE_ENV: 'production', PORT: 3000 },
-      error_file:   './logs/api-error.log',
-      out_file:     './logs/api-out.log',
-      merge_logs:   true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      exp_backoff_restart_delay: 100,
-    }
-  ]
+  apps: [{
+    name:               'macaroonie-api',
+    script:             './api/src/server.js',
+    instances:          'max',
+    exec_mode:          'cluster',
+    watch:              false,
+    max_memory_restart: '512M',
+    interpreter_args:   '--import ./api/src/preload.js',
+    env: {
+      NODE_ENV: 'production',
+      PORT:     3000,
+    },
+    error_file:      './logs/api-error.log',
+    out_file:        './logs/api-out.log',
+    merge_logs:      true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    exp_backoff_restart_delay: 100,
+  }]
 }
